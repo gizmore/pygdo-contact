974b4d1e840a7dff834e3c871fd7fcf821fd8d88
gizmore <gizmore@wechall.net>
2025-03-02 01:21:04 +0100

    'fixes'

--- test/test_contact.py
+++ test/test_contact.py
@@ -1,25 +1,23 @@
 import os
 import unittest
 
-from gdo.avatar.GDO_Avatar import GDO_Avatar
 from gdo.base.Application import Application
 from gdo.base.ModuleLoader import ModuleLoader
 from gdo.base.Util import module_enabled
 from gdo.core.GDO_Session import GDO_Session
-from gdotest.TestUtil import web_plug, reinstall_module, web_gizmore, install_module
+from gdotest.TestUtil import web_plug, reinstall_module, web_gizmore, install_module, GDOTestCase
 
-
-class AvatarTest(unittest.TestCase):
+class ContactTest(GDOTestCase):
 
     def setUp(self):
+        super().setUp()
         Application.init(os.path.dirname(__file__ + "/../../../../"))
         Application.init_cli()
         Application.set_session(GDO_Session.for_user(web_gizmore()))
         loader = ModuleLoader.instance()
         loader.load_modules_db()
-        loader.init_modules(load_vals=True)
+        loader.init_modules(True, True)
         install_module('contact')
-        return self
 
     def test_00_install(self):
         reinstall_module('contact')
@@ -28,6 +26,11 @@ class AvatarTest(unittest.TestCase):
     def test_01_form_rendering(self):
         web_gizmore()
         out = web_plug('contact.form.html').exec()
+        self.assertIn('gizmore', out, 'Staff link not shown on contact form.')
+
+    def test_02_contact(self):
+        web_gizmore()
+        out = web_plug('contact.form.html').post({'text': "test", 'submit': 1}).exec()
         self.assertIn('gizmore', out, 'Staff link not shown on contact form.')
 
 
